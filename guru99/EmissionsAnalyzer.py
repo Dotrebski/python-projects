653cd47605edf6049bfc02df54917e03366b41cd
Dominik Otrębski <130558948+Dotrebski@users.noreply.github.com>
2023-11-27 03:52:02 +0100

    Update EmissionsAnalyzer.py
    Cosmetic changes, mostly in docstrings and comments. Also, changed a few outputs to make them look neat.

--- guru99/EmissionsAnalyzer.py
+++ guru99/EmissionsAnalyzer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 """
-Emission Data CSV Analyzer
+Emission Data Analyzer by dotrebski
 
 This script allows the user to process and analyze a CSV file of a specific design in mind. The script:
- * verifies whether a valid (compliant with that specific design) CSV file exists;
- * initializes an object with values from that file enabling a convenient analysis;
- * analyzes any year in the file's range;
- * visualizes one to two countries using Matplotlib;
- * verifies whether the data in the initialized dictionary adheres to the specific design described below;
- * and finally extracts at least one country (and the associated data) to a subset CSV file.
+ * verifies whether a non-empty CSV file with the provided name exists;
+ * initializes an object with the data from that file facilitating analysis and other operations;
+ * analyzes any year in the file's range in terms of minimal, maximal and average values;
+ * visualizes the data related to one to two countries, using Matplotlib;
+ * verifies whether the data in the initialized object adheres to the specific design described below;
+ * and finally, extracts at least one country and the associated data into a subset CSV file.
 
 The script requires that `NumPy` and `Matplotlib` be installed within the Python environment the script is run in.
-Also, it imports the built-in `csv` and 'os' modules. According to vermin, the minimum required version of Python
-to run the script is 3.6.
+According to vermin, the minimum required version of Python to run the script is 3.6. This file can also be imported
+as a module.
 
 The specific design mentioned above is that:
  1) the first row should contain a range of years;
@@ -21,11 +21,9 @@ The specific design mentioned above is that:
  3) optionally, the first column of the first row should say what the file represents (e.g., CO2 per capita);
  4) the first column of the following rows should each contain a different country's name with proper capitalization.
 
-This file can also be imported as a module; it contains the main function of the script.
-
-Created by dotrebski in the course of participating in the guru99 Python project, with the constraint that
-`pandas` may not be used. As a **beginner**, I found it very challenging. However, in the end, it was
-a very enjoyable exercise, as well. I'm not planning on generalizing the design for the time being.
+Created in the course of the guru99 Python project, with the constraint that `pandas` could not be used
+in the implementation. As a **beginner**, I found it very challenging. However, in the end, it was a very satisfactory
+exercise, as well. I'm not planning on expanding the design at the moment.
 """
 
 import csv
@@ -42,25 +40,25 @@ class AnalysisCSV:
     Methods:
         @staticmethod
         get_file(prompt: str) -> str:
-            Prompts the user to enter a valid CSV file name and returns it.
+            Prompts the user to enter a CSV filename, checks if a non-empty file like that exists, and returns its name.
 
         __init__(self, filename: str) -> None:
-            Initialize an AnalysisCSV object with filename, data_dict, countries, first_key and first_value.
+            Initialize an object with the filename, data_dict, countries, first_key and first_value attributes.
 
         get_year(prompt: str) -> int:
-            Prompts the user to enter a valid year within the range of self.first_value and returns the year.
+            Prompts the user to enter a year in the range of self.first_value, inclusive. Returns the year.
 
         get_country(self, prompt: str, num: int, strict: bool = True) -> list[str]
-            Prompts the user to enter comma-separated list of valid countries and returns their literal list.
+            Prompts the user to enter a comma-separated string of countries and returns their list if they are correct.
 
         extract_to_file(self, countries_req: list, overwrite: bool = False) -> None
-            Extracts data associated with each country on the list and saves it into a new CSV file.
+            Extracts the data associated with each country on the list and saves it into a (new) CSV file.
 
         visualize(self, countries_req: list) -> None
-            Plots the countries on the list based on the data in self.data_dict, and displays the interactive plot.
+            Plots the data associated with the countries from the list. Displays an interactive plot.
 
         analyze_year(self, year: int) -> None
-            Finds and displays the countries with the min and max emission levels in the year, and the year's average.
+            Finds and displays the countries with the min and max emission levels in a year, and the year's average.
 
         verify_structure(self) -> bool
             Checks whether the data in the initialized dictionary is of the expected types.
@@ -69,27 +67,24 @@ class AnalysisCSV:
     @staticmethod
     def get_file(prompt: str) -> str:
         """
-        Prompts the user to enter a valid CSV file name and returns it.
+        Prompts the user to enter a CSV filename, checks if a non-empty file like that exists, and returns its name.
 
         Args:
-            prompt (str): The message to display to the user.
+            prompt (str): A message to display to the user.
 
         Returns:
-            str: The name of the CSV file entered by the user.
-
-        Raises:
-            FileNotFoundError: If the file does not exist.
+            str: The name of a non-empty CSV file.
         """
 
         while True:
             user_inp = input(prompt)
 
-            # If user's input doesn't include the '.csv' extension, append it.
+            # If user's input doesn't include the ".csv" extension, append it.
             if not user_inp.lower().endswith(".csv"):
                 user_inp += ".csv"
 
             try:
-                # Try to open the file. If it exists, get the number of rows in it and save it into a variable.
+                # Try to open the file. If it exists, save the number of rows in it into a variable.
                 with open(user_inp, newline="") as csvfile:
                     reader_len = len(list(csv.reader(csvfile)))
             except FileNotFoundError:
@@ -104,10 +99,10 @@ class AnalysisCSV:
 
     def __init__(self, filename: str) -> None:
         """
-        Initialize an AnalysisCSV object with filename, data_dict, countries, first_key and first_value.
+        Initialize an object with the filename, data_dict, countries, first_key and first_value attributes.
 
         Args:
-            filename (str): The valid filename of a CSV file.
+            filename (str): The name of a non-empty CSV file.
 
         Returns:
             None
@@ -117,7 +112,7 @@ class AnalysisCSV:
         with open(filename, newline="") as csvfile:
             reader = csv.reader(csvfile)
 
-            # Create a dictionary from the CSV file.
+            # Import the data from the file into a dictionary attribute.
             # The first element of each row is the key; the rest is the value.
             self.data_dict = {row.pop(0): row for row in reader}
 
@@ -128,20 +123,17 @@ class AnalysisCSV:
         self.first_key = self.countries.pop(0)
         self.first_value = self.data_dict.get(self.first_key)
 
-        print("All data has been read into a dictionary.\n")
+        print("All the data has been read into a dictionary.\n")
 
     def get_year(self, prompt: str) -> int:
         """
-        Prompts the user to enter a valid year within the range of self.first_value and returns the year.
+        Prompts the user to enter a year in the range of self.first_value, inclusive. Returns the year.
 
         Args:
-            prompt (str): The message to display to the user.
+            prompt (str): A message to display to the user.
 
         Returns:
             int: The year entered by the user.
-
-        Raises:
-            ValueError: If the year is not within the range of self.first_value.
         """
 
         while True:
@@ -155,6 +147,7 @@ class AnalysisCSV:
                 continue
 
             # Check whether the provided year is within the range of years in self.first_value.
+            # This is done under the assumption the elements in that attribute are sorted.
             if not int(self.first_value[0]) <= user_inp <= int(self.first_value[-1]):
                 print("You have provided an out-of-range year. Try again.")
                 continue
@@ -162,57 +155,58 @@ class AnalysisCSV:
 
     def get_country(self, prompt: str, num: int, strict: bool = True) -> list[str]:
         """
-        Prompts the user to enter comma-separated list of valid countries and returns their literal list.
+        Prompts the user to enter a comma-separated string of countries, and returns their list if they are correct.
 
         Args:
-            prompt (str): The message to display to the user.
-            num (int): The number of countries to use.
-            strict (bool): Whether to demand exactly the num or from 1 to num of the countries. Defaults to True.
+            prompt (str): A message to display to the user.
+            num (int): A number of countries to get.
+            strict (bool): If True, expects exactly the num of countries; else, from 1 to num. Defaults to True.
 
         Returns:
             list[str]: The list of countries.
         """
 
         while True:
-            # Split the user's input with a comma delimiter.
             user_inp = input(prompt).split(",")
             user_inp_len = len(user_inp)
 
             # Check if the length of the above list is as demanded by the num and strict arguments.
             if (strict and user_inp_len == num) or (not strict and 0 < user_inp_len <= num):
                 # Strip each item in the list of whitespace on each side and capitalize each word in the item.
-                # Also, lower the case of the word 'And' if it exists in the item.
+                # Also, lower the case of some words if they exists in the item.
                 user_inp = [c.strip().title().replace(" And ", " and ").replace("D'", "d'")
                             for c in user_inp]
 
-                # Check for duplicates using a set.
+                # Check for any duplicates using a set.
                 if len(set(user_inp)) < user_inp_len:
                     print("You have entered some duplicate countries. Try again.")
                     continue
 
-                # Check whether all the items in the list correspond with the countries in self.countries.
+                # Check whether all the items in the list correspond are found in self.countries.
                 if not all([c in self.countries for c in user_inp]):
                     print("You have provided some incorrect names of countries. Try again.")
                     continue
+
                 return user_inp
-            print(f"The provided input doesn't match the arguments (num: {num}, strict: {strict}). Try again.")
+            print(f"The provided input is unexpected (you must enter {('from 1 to ', 'exactly')[strict]} {num}"
+                  f" of countries). Try again.")
 
     def extract_to_file(self, countries_req: list, overwrite: bool = False) -> None:
         """
-        Extracts the data associated with each country on the list and saves it into a new CSV file.
+        Extracts the data associated with each country on the list and saves it into a (new) CSV file.
 
         Args:
-            countries_req (list): The list of countries to extract.
-            
+            countries_req (list): A list of countries to extract.
             overwrite (bool): Whether to overwrite the [filename]_subset.csv file. Defaults to False.
+
         Returns:
             None
         """
 
-        # Prepare the name for the subset by appending '_subset.csv' to the name of the original CSV file.
+        # Prepare the name for the subset by appending '_subset.csv' to the original CSV filename.
         subset_filename = self.filename.replace(".csv", "_subset.csv")
 
-        # If the overwrite parameter is False and the file already exist, incrementally find the closest free name.
+        # If the overwrite argument is False and the file already exist, incrementally find the closest free name.
         if not overwrite and exists(subset_filename):
             counter = 0
             name, ext = splitext(subset_filename)
@@ -235,10 +229,10 @@ class AnalysisCSV:
 
     def visualize(self, countries_req: list) -> None:
         """
-        Plots the countries on the list based on the data in self.data_dict, and displays the interactive plot.
+        Plots the data associated with the countries from the list. Displays an interactive plot.
 
         Args:
-            countries_req (list): The list of countries to visualize.
+            countries_req (list): A list of countries to visualize.
 
         Returns:
             None
@@ -270,23 +264,20 @@ class AnalysisCSV:
         plt.xlabel("Year")
 
         # Set the y-axis label depending on the number of countries.
+        plt.ylabel(f"Emissions in {', '.join(countries_req)}")
         if len(countries_req) > 1:
-            plt.ylabel("Emissions")
-
-            # Add a legend to show the country names.
+            # Add a legend to show the countries' names.
             ax.legend()
-        else:
-            plt.ylabel(f"Emissions in {countries_req[0]}")
 
         # Show the plot.
         plt.show()
 
     def analyze_year(self, year: int) -> None:
         """
-        Finds and displays the countries with the min and max emission levels in the year, and the year's average.
+        Finds and displays the countries with the min and max emission levels in a year, and the year's average.
 
         Args:
-            year (int): The year to analyze.
+            year (int): A year to analyze.
 
         Returns:
             None
@@ -312,9 +303,6 @@ class AnalysisCSV:
 
         Returns:
             bool
-
-        Raises:
-            ValueError: If the dictionary doesn't conform to the design described in the module's docstring.
         """
 
         # Enumerate the items in the dictionary to have an easier way of tracking the index.
@@ -345,8 +333,7 @@ class AnalysisCSV:
 def main():
     csv_obj = AnalysisCSV(AnalysisCSV.get_file("Provide the name of the file CSV you wish to read data from: "))
 
-    # It may be unnecessary for the moment.
-    # print(f"Is the file structured as expected:\t{csv_obj.verify_structure()}")
+    # print(f"Is the file structured as expected? {('No', 'Yes')[csv_obj.verify_structure()]}.")
     csv_obj.analyze_year(csv_obj.get_year(f"Select a year to find statistics "
                                           f"({csv_obj.first_value[0]}-{csv_obj.first_value[-1]}): "))
     csv_obj.visualize(csv_obj.get_country("Select a country to visualize: ", 1))
